80b10e4e94fda54040815302ec1ef07c06d106d3
nnleaf <nn.hoang.nguyen@gmail.com>
2021-06-04 10:56:28 -0400

    Update start.sh

--- SCRIPT/start.sh
+++ SCRIPT/start.sh
@@ -8,5 +8,5 @@
 #Nam - 20210604
 #Adds cronjob and reboots
 #
-sudo echo "@reboot /tmp/SCRIPT/script.sh" >> /var/spool/cron/root
+sudo echo "@reboot /tmp/SCRIPT/script.sh" >> /var/spool/cron/crontabs/root
 sudo reboot
