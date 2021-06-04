f6d0fa6f189e818b48b45790ab17ba77814a9bf3
nnleaf <nn.hoang.nguyen@gmail.com>
2021-06-04 12:02:38 -0400

    added more tools

--- SCRIPT/script.sh
+++ SCRIPT/script.sh
@@ -7,7 +7,10 @@
 #//     ////  //     ////
 #Nam - 20210604
 #Debian basic scripted install
-#
+
+#Variables
+home="/home/nn"
+
 #Add contrib and non-free sources
 sed -i '/security.debian/d' >> /etc/apt/sources.list
 echo "deb http://security.debian.org/ buster/updates main contrib non-free" >> /etc/apt/sources.list
@@ -15,12 +18,18 @@ echo "deb-src http://security.debian.org/ buster/updates main contrib non-free" 
 sed -i '/deb.debian/d' >> /etc/apt/sources.list
 echo "deb  http://deb.debian.org/debian buster main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src  http://deb.debian.org/debian buster main contrib non-free" >> /etc/apt/sources.list
+#Add VMWareTool source
+echo "deb http://ftp.debian.org/debian/ buster main contrib" >> /etc/apt/sources.list
 
 #Update Debian
 apt update -y
 
 #Install Packages
-apt install -y intel-microcode neofetch
+apt install -y intel-microcode neofetch open-vm-tools tmux mlocate mc
 
 #Configure Neofetch
 echo "neofetch" >> /etc/bash.bashrc
+cp -r $home/SCRIPT/neofetch/ $home/.config/.
+
+#Update mlocate database
+updatedb
--- SCRIPT/start.sh
+++ SCRIPT/start.sh
@@ -7,6 +7,9 @@
 #//     ////  //     ////
 #Nam - 20210604
 #Adds cronjob and reboots
-#
-sudo echo "@reboot /bin/sh /home/nn/SCRIPT/script.sh" >> /var/spool/cron/crontabs/root
+
+#Variables
+home="/home/nn"
+
+sudo echo "@reboot /bin/sh $home/SCRIPT/script.sh" >> /var/spool/cron/crontabs/root
 sudo reboot
