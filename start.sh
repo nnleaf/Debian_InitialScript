595ce4f63a23eb8e7a2b814ef05a0dad5eb98f39
nnleaf <32043611+nnleaf@users.noreply.github.com>
2021-09-04 10:42:55 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,8 +19,8 @@ echo "deb-src http://deb.debian.org/debian/ bullseye main contrib non-free" >> /
 echo "deb http://deb.debian.org/debian/ bullseye-updates main" >> /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian/ bullseye-updates main" >> /etc/apt/sources.list
 sed -i '/security.debian/d' /etc/apt/sources.list
-echo "deb http://security.debian.org/debian-security bullseye/updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://security.debian.org/debian-security bullseye/updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://security.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list
 apt update -y
 
 #Intel microcode
